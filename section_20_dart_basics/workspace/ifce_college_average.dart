12d42c8fb44a3a61d60a64c894ef113c26a90c70
Danilo de Sousa <45634291+danilocdesousa@users.noreply.github.com>
2024-09-16 13:52:17 -0300

    Create ifce_college_average.dart

---
+++ section_20_dart_basics/workspace/ifce_college_average.dart
@@ -0,0 +1,12 @@
+void main() {
+  const double class1 = 7.0;
+  const double class2 = 9.0;
+  const double class3 = 7.0;
+  const double class4 = 5.7;
+
+  const n1Average = (class1 + class2) / 2;
+  const n2Average = (class3 + class4) / 2;
+
+  const finalAverage = (n1Average * 2 + n2Average * 3) / 5;
+  print(finalAverage);
+}
