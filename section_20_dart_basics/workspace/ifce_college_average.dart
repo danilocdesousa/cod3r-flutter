182ffc81d58d658fc00e42939fe20ba8dbe972e1
Danilo de Sousa <45634291+danilocdesousa@users.noreply.github.com>
2024-09-27 23:01:02 -0300

    Update ifce_college_average.dart

--- section_20_dart_basics/workspace/ifce_college_average.dart
+++ section_20_dart_basics/workspace/ifce_college_average.dart
@@ -2,11 +2,16 @@ void main() {
   const double class1 = 7.0;
   const double class2 = 9.0;
   const double class3 = 7.0;
-  const double class4 = 5.7;
+  const double class4 = 2.5;
 
   const n1Average = (class1 + class2) / 2;
   const n2Average = (class3 + class4) / 2;
 
-  const finalAverage = (n1Average * 2 + n2Average * 3) / 5;
-  print(finalAverage);
+  print(n1Average);
+  print(n2Average);
+
+  const finalAverage1 = (8 * 2 + 4.8 * 3) / 5;
+  const finalAverage2 = (n1Average * 2 + n2Average * 3) / 5;
+  print("Média 1: " + finalAverage1.toString());
+  print("Média 2: ${finalAverage2}");
 }
