d112875b881df78fbfa73cf76d1546ac3828a7c1
Danilo de Sousa <45634291+danilocdesousa@users.noreply.github.com>
2024-10-06 15:56:42 -0300

    Create built_in_types.dart

---
+++ section_20_dart_basics/workspace/built_in_types.dart
@@ -0,0 +1,17 @@
+/*
+  # Numbers
+  - int
+  - double
+
+  # Strings
+
+  # Booleans
+ */
+
+void main() {
+  int intValue1 = 3;
+  double doubleValue1 = -5.67;
+  double doubleValue2 = double.parse('1.23');
+
+  print(intValue1 + doubleValue1.abs() + doubleValue2);
+}
