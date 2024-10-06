467571a23ae411a46255cea43254501374f1b4fc
Danilo de Sousa <45634291+danilocdesousa@users.noreply.github.com>
2024-10-05 23:44:38 -0300

    Update constants_1.dart

--- section_20_dart_basics/workspace/constants_1.dart
+++ section_20_dart_basics/workspace/constants_1.dart
@@ -4,12 +4,12 @@ void main() {
   // The area of a circle: PI * r * r;
   const double PI = 3.1415;
 
-  stdout.write("Digite o valor do raio: ");
+  stdout.write("Digite o valor do raio do círculo: ");
 
   final userInput = stdin.readLineSync()!;
-  final ray = double.parse(userInput);
-  final area = PI * ray * ray;
+  final circleRay = double.parse(userInput);
+  final circleArea = PI * circleRay * circleRay;
 
-  print("O valor do raio é ${ray}");
-  print("O valor da área é ${area}");
+  print("O valor do raio é ${circleRay}");
+  print("O valor da área é ${circleArea}");
 }
